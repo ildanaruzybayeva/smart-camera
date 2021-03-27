7b7355c101f627d58aab988c1bb53127f2aaa431
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2021-03-27 15:24:44 +0100

    Added tf object detection to tab 2

---
+++ src/hooks/PhotoGallery.js
@@ -0,0 +1,96 @@
+import React, { useRef, useState, useEffect } from "react";
+import Webcam from "react-webcam";
+import { useCamera } from '@ionic/react-hooks/camera';
+import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem';
+import { useStorage } from '@ionic/react-hooks/storage';
+import { isPlatform } from '@ionic/react';
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import { drawRect } from "./utilities";
+import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
+
+export function PhotoGallery() {
+  const [object, setObject] = useState()
+
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    console.log("Handpose model loaded.");
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const obj = await net.detect(video);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawRect(obj, ctx);
+    }
+  };
+
+  useEffect(() => { runCoco() }, []);
+
+  const videoConstraints = {
+    facingMode: { exact: "environment" }
+  };
+
+  return (
+    <header className="App-header">
+      <h1>{object}</h1>
+      <Webcam
+        videoConstraints={videoConstraints}
+        ref={webcamRef}
+        muted={true}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+        }}
+      />
+
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 8,
+          width: 640,
+          height: 480,
+        }}
+      />
+    </header>
+  )
+}
